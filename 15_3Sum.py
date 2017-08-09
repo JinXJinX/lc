c244ddddcea9cb004d7cdfed6820a4fedd9d3cfc
Xu Jing <jingxu086@gmail.com>
2017-08-09 01:18:06 -0400

    15

---
+++ 15_3Sum.py
@@ -0,0 +1,31 @@
+# 313 / 313 test cases passed.
+# Status: Accepted
+# Runtime: 1035 ms
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        for i in range(len(nums) - 2):
+            #reduce redundancy step
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    #reduce redundancy step
+                    while l < r and nums[l] == nums[l+1]: l += 1
+                    while l < r and nums[r] == nums[r-1]: r -= 1
+                    l += 1
+                    r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    r -= 1
+        return res
