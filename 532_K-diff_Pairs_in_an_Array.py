4cd94598cfd992e656af232da49100b55f538fd4
Xu Jing <jingxu086@gmail.com>
2017-08-16 00:18:19 -0400

    532

---
+++ 532_K-diff_Pairs_in_an_Array.py
@@ -0,0 +1,17 @@
+# 72 / 72 test cases passed.
+# Status: Accepted
+# Runtime: 52 ms
+
+class Solution(object):
+    def findPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if k>0:
+            return len(set(nums)&set(n+k for n in nums))
+        elif k==0:
+            return sum(v>1 for v in collections.Counter(nums).values())
+        else:
+            return 0
