c69df8e7e7fb78cc6012f337ef2d375b8a162545
Xu Jing <jingxu086@gmail.com>
2017-08-16 23:57:26 -0400

    81

---
+++ 81_Search_in_Rotated_Sorted_Array_II.py
@@ -0,0 +1,13 @@
+# 273 / 273 test cases passed.
+# Status: Accepted
+# Runtime: 65 ms
+
+# this solution is so doof
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        return target in nums
