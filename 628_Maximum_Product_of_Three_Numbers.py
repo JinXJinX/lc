c4b196b8c613288afb9ed7126d8cd4c6dca7b9d7
Xu Jing <jingxu086@gmail.com>
2017-08-20 02:12:45 -0400

    628

---
+++ 628_Maximum_Product_of_Three_Numbers.py
@@ -0,0 +1,12 @@
+# 83 / 83 test cases passed.
+# Status: Accepted
+# Runtime: 112 ms
+
+class Solution(object):
+    def maximumProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return max(nums[-1] * nums[-2] * nums[-3], nums[0] * nums[1] * nums[-1])
