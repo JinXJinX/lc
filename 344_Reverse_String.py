34dd76fa6d83a8ab4179231ae73608516bf60661
Xu Jing <jingxu086@gmail.com>
2017-08-25 19:38:20 -0400

    344

---
+++ 344_Reverse_String.py
@@ -0,0 +1,12 @@
+# 476 / 476 test cases passed.
+# Status: Accepted
+# Runtime: 39 ms
+
+# life is short, you need python
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
