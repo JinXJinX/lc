9a878d94ad6b5452ec16f7956a9b4464062d97b0
Xu Jing <jingxu086@gmail.com>
2017-08-29 15:07:26 -0400

    434

---
+++ 434_Number_of_Segments_in_a_String.py
@@ -0,0 +1,11 @@
+# 26 / 26 test cases passed.
+# Status: Accepted
+# Runtime: 32 ms
+
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.split())
