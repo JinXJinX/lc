a8d6d5e91050d5eb8b67bf92c598249297d0ab2b
Xu Jing <jingxu086@gmail.com>
2017-09-06 03:01:14 -0400

    482

---
+++ leetcode/482_License_Key_Formatting.py
@@ -0,0 +1,19 @@
+# 35 / 35 test cases passed.
+# Status: Accepted
+# Runtime: 42 ms
+
+class Solution(object):
+    def licenseKeyFormatting(self, S, K):
+        """
+        :type S: str
+        :type K: int
+        :rtype: str
+        """
+        S = S.upper().replace('-','')
+        size = len(S)
+        s1 = K if size%K==0 else size%K
+        res = S[:s1]
+        while s1<size:
+            res += '-'+S[s1:s1+K]
+            s1 += K
+        return res
